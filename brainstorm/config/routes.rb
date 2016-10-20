59652cba294a06a6f1429bf98fec921b80a979dc
Gabriel Estes <gabriel.estes@gmail.com>
2016-10-20 18:05:41 -0400

    Specify registration form as root route

--- brainstorm/config/routes.rb
+++ brainstorm/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # root 'users/sessions#new'
   devise_scope :user do
-    get '/' => 'users/sessions#new'
+    root to: 'users/sessions#new'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
