5c71968d8d3300773acc25325a8bdc0c44289b74
Gabriel Estes <gabriel.estes@gmail.com>
2016-10-20 17:56:13 -0400

    Create devise controllers,create route from registration

--- brainstorm/config/routes.rb
+++ brainstorm/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+
+  # root 'users/sessions#new'
+  devise_scope :user do
+    get '/' => 'users/sessions#new'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
