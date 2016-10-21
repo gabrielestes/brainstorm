fe6ace57be7e289990aa2f2bf0f14869d72abbaf
Gabriel Estes <gabriel.estes@gmail.com>
2016-10-21 09:59:45 -0400

    Move css into assets

--- brainstorm/config/routes.rb
+++ brainstorm/config/routes.rb
@@ -3,9 +3,21 @@ Rails.application.routes.draw do
         sessions: 'users/sessions'
       }
 
-  # root 'users/sessions#new'
+  # root is defaulted to login/register
   devise_scope :user do
     root to: 'users/sessions#new'
   end
+
+  # displays new game
+  get '/game'
+
+  # displays top x scores by players
+  get '/leaderboard'
+
+  # displays user's game history and account info
+  # get profile
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
