3a14f11c20b421092c186ff7f5b8b80269e380f4
Lindsey Adams <linzma87@gmail.com>
2016-10-20 21:57:12 -0400

    Transitions/edit-info feature added to profile page

--- brainstorm-dev/libdev/js/profile.js
+++ brainstorm-dev/libdev/js/profile.js
@@ -1,2 +1,12 @@
-"use strict";
+'use strict';
+
+$('.profile-container').on('click', 'img.edit', function () {
+    $('.edit-info, .profile-container').addClass('active');
+    $('.user-info').addClass('inactive');
+});
+$('.profile-container').on('click', 'img.save', function () {
+    $('form').submit();
+    $('.edit-info').removeClass('active');
+    $('.user-info').removeClass('inactive');
+});
 //# sourceMappingURL=profile.js.map
