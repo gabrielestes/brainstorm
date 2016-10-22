f93b2ce89a57c8617724128b64eb384168285a81
Lindsey Adams <linzma87@gmail.com>
2016-10-22 14:24:41 -0400

    Work on responsiveness

--- brainstorm-dev/libdev/js/leaderboard.js
+++ brainstorm-dev/libdev/js/leaderboard.js
@@ -2,9 +2,24 @@
 
 $(document).ready(function () {
 
-  $('header').on('click', 'a', function () {
+  $('nav.menu').on('click', 'a', function () {
     $('a').removeClass('active');
     $(this).addClass('active');
   });
+
+  $('header').on('click', 'a.menu', function () {
+    $(this).toggleClass('active');
+    $('.drop-nav').toggleClass('active');
+  });
+
+  function getRank() {
+    $('li.player:nth-child(1)').children('p.rank').text('1');
+    $('li.player:nth-child(2)').children('p.rank').text('2');
+    $('li.player:nth-child(3)').children('p.rank').text('3');
+    $('li.player:nth-child(4)').children('p.rank').text('4');
+    $('li.player:nth-child(5)').children('p.rank').text('5');
+  }
+
+  getRank();
 });
 //# sourceMappingURL=leaderboard.js.map
